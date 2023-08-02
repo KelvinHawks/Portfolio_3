aa36b453643e8705481e0f1a20576a7b651e8e2d
kimdev <kevohkyme35@gmail.com>
2023-08-02 12:01:26 +0300

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      hello
+     
+    </div>
+  );
+}
+
+export default App;
