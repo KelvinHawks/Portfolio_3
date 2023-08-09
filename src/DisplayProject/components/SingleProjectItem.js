7b8fc38048cc79050688825531fe5b81b7dc01f4
kimdev <kevohkyme35@gmail.com>
2023-08-09 15:36:04 +0300

    add buttons

--- src/DisplayProject/components/SingleProjectItem.js
+++ src/DisplayProject/components/SingleProjectItem.js
@@ -9,8 +9,12 @@ function SingleProjectItem(props) {
         <h1>{props.image}</h1>
         <h2>{props.title}</h2>
         <p>{props.description}</p>
-        <Button href inverse>VIEW IN WEB</Button>
-        <Button Link inverse>VIEW CODE</Button>
+        <div className='place-items__buttons'>
+          <Button href inverse>VIEW IN WEB</Button>
+          <Button to Link inverse>VIEW CODE</Button>
+          <Button to danger>Delete</Button>
+        </div>
+        
       </Card>
     </li>
   )
--- src/shared/formElements/Button.js
+++ src/shared/formElements/Button.js
@@ -12,9 +12,12 @@ function Button(props) {
     }
     if(props.to){
   return (
-    <Link to={props.to} exact={props.exact} className={`button button--${props.size || 'default'} ${props.inverse && 'button-inverse'} ${props.danger && 'button--danger'}`}></Link>
+    <Link to={props.to} exact={props.exact} className={`button button--${props.size || 'default'} ${props.inverse && 'button-inverse'} ${props.danger && 'button--danger'}`}>{props.children}</Link>
   )
 }
+return(
+  <button className={`button button--${props.size || 'default'} ${props.inverse && 'button-inverse'} ${props.danger && 'button-danger'}`} type={props.type} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>
+)
 }
 
 export default Button
