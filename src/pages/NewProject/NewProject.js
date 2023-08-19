278f23a717e6e761d97113c9a1c493b736fa9a7d
kimdev <kevohkyme35@gmail.com>
2023-08-19 13:46:09 +0300

    Reducer to capture inputs

--- src/pages/NewProject/NewProject.js
+++ src/pages/NewProject/NewProject.js
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, { useCallback, useReducer } from 'react'
 import Input from '../../shared/formElements/Input'
 import './NewProject.css'
+import Button from '../../shared/formElements/Button'
+
+const formReducer = (state, action)=>{
+  switch(action.type){
+    case 'INPUT_CHANGE':
+      let formIsValid = true
+      for(const inputId in action.inputId){
+        if(inputId === action.inputId){
+          formIsValid = formIsValid && action.isValid
+        }else{
+          formIsValid = formIsValid && state.inputs[inputId].isValid
+        }
+        return{
+          ...state,
+          inputs:{
+            ...state.inputs,
+            [action.inputId]:{value:action.value, isValid:action.isValid}
+          },
+          isValid:formIsValid
+        }
+        
+      }
+      default:
+        return state
+  }
+  
+
+}
 function NewProject() {
+  const[formState, dispatch] = useReducer(formReducer,{
+    inputs:{
+    title:{
+      value:'',
+      isValid:false
+    },
+    description:{
+      value:'',
+      isValid:false
+    }
+  },
+    isValid:false
+  })
+  const inputHandler = useCallback((id,value,isValid)=>{
+    dispatch({type:'INPUT_CHANGE',
+    value:value,
+    isValid:isValid,
+    inputId:id
+  })
+  },[dispatch])
+  const placeSubmitHandler = (e)=>{
+    e.preventDefault()
+    console.log(formState.inputs);
+  }
   return (
-    <form className='place-form'>
-        <Input element='input' id='title' label='Title' type='text' placeholder='Title'/>
-        <Input id='description' label='Description' type='text'/>
+    <form className='place-form' onSubmit={placeSubmitHandler}>
+        <Input 
+          id='title' 
+          element='input' 
+          label='Title' 
+          type='text' 
+          placeholder='Title'
+          onInput={inputHandler}
+        />
+        <Input 
+          id='description' 
+          label='Description'
+          onInput={inputHandler} 
+        />
+        <Input 
+          id='address' 
+          element='input'  
+          label='Address' 
+          type='text' 
+          placeholder='Address'
+          onInput={inputHandler}
+        />
+        <Button inverse>ADD PLACE</Button>
     </form>
   )
 }
--- src/shared/formElements/Input.js
+++ src/shared/formElements/Input.js
@@ -3,8 +3,18 @@ import './Input.css'
 
 function Input(props) {
     const element = props.element === 'input'? 
-        <input id={props.id} placeholder={props.placeholder} type={props.type} value={props.value || ''}/> :
-         <textarea id={props.id} rows={props.rows || 3} type={props.type} value={props.value}/>
+        <input 
+          id={props.id} 
+          placeholder={props.placeholder} 
+          type={props.type} 
+          
+        /> :
+         <textarea 
+          id={props.id} 
+          rows={props.rows || 3} 
+          type={props.type} 
+          
+        />
   return (
     <div className='form-control'>
         <label htmlFor={props.id}>{props.label}</label>
